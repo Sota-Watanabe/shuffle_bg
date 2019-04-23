17fc48fd876b5038276090518e8e8c03d7b0eccb
Sota-Watanabe <cy16179@shibaura-it.ac.jp>
2019-04-23 19:24:02 +0900

    delete double

--- shuffle_bg.sh
+++ shuffle_bg.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 thumbnails='/home/sota/Pictures/wallpaper/'
+if [ "1" = $(pgrep -c $(basename $0 .sh)) ]; then
+    echo "start" >> a.txt
+    while :
+        do
+        images=(`ls $thumbnails`)
+        num_images=${#images[*]}
+        img_path1=$thumbnails${images[$((RANDOM%num_images))]}
+        img_path2=$thumbnails${images[$((RANDOM%num_images))]} 
 
-while :
-do
-    images=(`ls $thumbnails`)
-    num_images=${#images[*]}
-    img_path1=$thumbnails${images[$((RANDOM%num_images))]}
-    img_path2=$thumbnails${images[$((RANDOM%num_images))]}
+        flatpak run org.gabmus.hydrapaper -c ${img_path1} ${img_path2} 
+        sleep 30;
+    done
+fi
 
-    flatpak run org.gabmus.hydrapaper -c ${img_path1} ${img_path2} 
-    sleep 30;
-done 
